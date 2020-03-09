152b4e392ee69e867491670adf33f94aea14e09c
MoloshniyV <MoloshniyV@gmail.com>
2020-03-09 17:00:18 +0200

    simplest version of Iterable and Iterator Implementation

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.education
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import kotlin.properties.Delegates
 
@@ -11,10 +12,24 @@ import kotlin.properties.Delegates
 //
 class MainActivity : AppCompatActivity() {
 
- val myRange = Date(1997,12)..Date(1998,11)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dates:Dates = Dates()
+       dates.addElement(Date(1))
+        dates.addElement(Date(2))
+        dates.addElement(Date(3))
+        for (d in dates){
+                Log.e("qqq",d.int.toString())
+        }
+        for (d in dates){
+            Log.e("qqq",d.int.toString())
+        }
+        for (d in dates){
+            Log.e("qqq",d.int.toString())
+        }
+
+
     }
 }
