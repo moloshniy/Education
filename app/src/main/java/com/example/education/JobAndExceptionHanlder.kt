46ad64b09f12367f72350d8ccfe69ab0de95f4a7
Viktor Moloshnyi <moloshniyV@gmail.com>
2020-03-23 16:32:30 +0200

    coroutine Job vs SuperVisorJob    context and Scoope    -  error propagation   cancel error propagation

--- app/src/main/java/com/example/education/JobAndExceptionHanlder.kt
+++ app/src/main/java/com/example/education/JobAndExceptionHanlder.kt
@@ -4,43 +4,93 @@ import android.util.Log
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
-class JobStacter : CoroutineScope {
+/** IF we have Hierarhy if
+        Job 1-1
+        /
+    Job 1
+   /     \
+Job        Job 1-2
+    \
+     \      Job 2-1
+      \   /
+       Job 2
+        \
+        Job 2-2
+
+If we have crash in any job and have Exception handler in main Job
+ Every job will crash !!
+
+ Если ошибка случилась в 1-2   джобе   то все остальные джобы тоже прекратят работу
+
+2020-03-23 11:17:53.959 2389-2469/com.example.education E/qqq: child - 1.2
+2020-03-23 11:17:53.959 2389-2467/com.example.education E/qqq: child - 1.1
+2020-03-23 11:17:53.959 2389-2467/com.example.education E/qqq: child - 2.1
+2020-03-23 11:17:54.953 2389-2469/com.example.education E/qqq: child - 2.2
+2020-03-23 11:17:54.956 2389-2467/com.example.education E/qqq: exception handled
+
+
+ */
+
+class JobAndExceptionHanlder : CoroutineScope {
 
     private val job = Job()
-    private lateinit var parentJob:Job
-    private lateinit var childJob1:Job
-    private lateinit var childJob2:Job
+    private lateinit var parentJob: Job
+    private lateinit var childJob1: Job
+    private lateinit var childJob1_1: Job
+    private lateinit var childJob1_2: Job
+    private lateinit var childJob2: Job
+    private lateinit var childJob2_1: Job
+    private lateinit var childJob2_2: Job
 
-    private val exHandler = CoroutineExceptionHandler{c,e->
-        Log.e("qqq","exception handled")
+    private val exHandler = CoroutineExceptionHandler { c, e ->
+        Log.e("qqq", "exception handled")
     }
 
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.Default + exHandler
 
     fun start() {
-         parentJob = launch {
+        parentJob = launch {
 
-                 childJob1 = launch(parentJob) {
-                     logs("child1")
+            childJob1 = launch {
 
-                 }
+                childJob1_1 = launch {
+                    while (isActive) {
+                        delay(2000)
+                        Log.e("qqq", "child - 1.1")
 
-                 childJob2 = launch(parentJob) {
-                     logs("child2", true)
-                 }
-             Log.e("qqq", " p c  = ${parentJob.children}")
+                    }
+                }
 
-             logs("parentJob")
-         }
+                childJob1_2 = launch {
+                    while (isActive) {
+                        delay(2000)
+                        Log.e("qqq", "child - 1.2")
 
-    }
+                    }
+                }
+            }
 
-    suspend fun logs(name: String,ex:Boolean = false) {
-        repeat(5) {
-            delay(1500)
-            Log.e("qqq", name)
-            if (ex && it==1) throw Exception()
+            childJob2 = launch {
+
+                childJob2_1 = launch {
+                    while (isActive) {
+                        delay(2000)
+                        Log.e("qqq", "child - 2.1")
+
+                    }
+                }
+
+                childJob2_2 = launch {
+                    while (isActive) {
+                        delay(3000)
+                        Log.e("qqq", "child - 2.2")
+
+                        throw Exception()
+                    }
+                }
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.education
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import kotlin.properties.Delegates
+import androidx.appcompat.app.AppCompatActivity
 
 
 //Simple example of using ClosedRange Interface
@@ -15,12 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val jobStacker = SupervisorJob_JobAndCancalationHanlder()
+            jobStacker.start()
 
-        Car.CarBuilder().addCarColor("green").build()
-        Car2.addWheel(1).addCarColor("2").build()
-        val c3:Car3 = Car3.builder {
-            this.addAge(11)
-            this.addName(11)
-        }
     }
 }
+
