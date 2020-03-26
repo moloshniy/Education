51add5238cd81d2f4f5652cac8443c4668295a03
Viktor Moloshnyi <moloshniyV@gmail.com>
2020-03-26 13:59:34 +0200

    charley

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.education
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Retrofit
 
 
 //Simple example of using ClosedRange Interface
@@ -11,9 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-            val jobStacker = SupervisorJob_JobAndCancalationHanlder()
-            jobStacker.start()
-
+        val retrofitBuilder = Retrofit.Builder().baseUrl("http://mysite.com")
+        val rt = retrofitBuilder.build()
+        val ss = rt.create(ApiClient::class.java)
+        ss.sentRequest("3232")
     }
 }
 
