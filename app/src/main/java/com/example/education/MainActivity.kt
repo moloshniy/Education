24438365211dd61afa4e7ed3b9b7312f9a211f8e
Viktor Moloshnyi <moloshniyV@gmail.com>
2020-04-30 14:30:08 +0300

    initial

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -8,29 +8,14 @@ import android.os.Bundle
 
 import android.view.View
 import androidx.annotation.RequiresApi
-import com.example.education.notification.NotificationExample
 
 
 class MainActivity : AppCompatActivity() {
-    val mediaIntent1:Intent by lazy { Intent(this, MediaService::class.java)}
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-       // NotificationExample().start(this)
-
-    }
-
-    fun onStart(v: View){
-        val mediaIntent:Intent  = Intent(this, MediaService::class.java)
-        startService(mediaIntent)
-       // startForegroundService(mediaIntent)
-    }
-
-    fun onStop(v: View){
-        val mediaIntent:Intent  = Intent(this, MediaService::class.java)
-        stopService(mediaIntent)
     }
 
 }
